181ae90c9088ec5bbc11e5206921986b3be7824e
Dhrubodh <dhrubo.talukder@smoothstack.com>
2021-08-24 12:16:56 -0400

    added sprites to the layer

--- game.js
+++ game.js
@@ -10,8 +10,8 @@ loadRoot('https://i.imgur.com/')
 loadSprite('coin', 'wbKxhcd.png')
 loadSprite('evil-shroom', 'KPO3fR9.png')
 loadSprite('brick', 'pogC9x5.png')
-loadSprite('block', 'bdrLpi6.png')
-loadSprite('mario', 'Wb1qfhk.png')
+loadSprite('block', 'M6rwarW.png')
+loadSprite('mario', 'Wb1qfhK.png')
 loadSprite('mushroom', '0wMd92p.png')
 loadSprite('surprise', 'gesQ1KP.png')
 loadSprite('unboxed', 'bdrLpi6.png')
@@ -22,6 +22,43 @@ loadSprite('pipe-bottom-right', 'nqQ79eI.png')
 
 scene("game", () => {
     layers(['bg', 'obj', 'ui'], 'obj')
+
+    const map = [
+        '                                         ',
+        '                                         ',
+        '                                         ',
+        '                                         ',
+        '                                         ',
+        '       %   =*=%=                         ',
+        '                                         ',
+        '                               -+        ',
+        '                    ^     ^    ()        ',
+        '=================================   ====='
+    ]
+
+    const levelCfg = {
+        width: 20,
+        height: 20,
+        '=': [sprite('block'), solid()],
+        '$': [sprite('coin')],
+        '%': [sprite('surprise'), solid()],
+        '*': [sprite('surprise'), solid()],
+        '}': [sprite('unboxed'), solid()],
+        '(': [sprite('pipe-bottom-left'), solid(), scale(0.5)],
+        ')': [sprite('pipe-bottom-right'), solid(), scale(0.5)],
+        '-': [sprite('pipe-top-left'), solid(), scale(0.5)],
+        '+': [sprite('pipe-top-right'), solid(), scale(0.5)],
+        '^': [sprite('evil-shroom'), solid()]
+    }
+
+    const gameLevel = addLevel(map, levelCfg)
+
+    const player = add([
+        sprite('mario'), solid(),
+        pos(30, 0),
+        body(),
+        origin('bot')
+    ])
 })
 
 start("game")
